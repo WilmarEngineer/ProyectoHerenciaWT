4d4522b9fa5413821b9c20a2a237dcf4e488c0ae
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-08-27 14:51:10 -0500

    Creacion de la Subclase Estudiante que hereda de la Superclase Persona

--- src/herenciawt/Persona.java
+++ src/herenciawt/Persona.java
@@ -6,22 +6,17 @@ public class Persona {
     
     private String nombre ="";
     private int edad=0; 
-    
+ 
     public String getNombre(){
         return nombre;
     }
-    
     public void setNombre(String nombre){
         this.nombre = nombre;
     }
-    
     public int getEdad(){
         return edad;
     }
-    
     public void setEdad(int edad){
         this.edad = edad;
     }
-    
-    
 }
