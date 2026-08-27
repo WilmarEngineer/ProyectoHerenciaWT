46aae2c3d87983c72978e787e55b8b793c9fbf85
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-08-27 15:24:13 -0500

    Definicion de valores en atributos de estudiante1

--- src/herenciawt/HerenciaWT.java
+++ src/herenciawt/HerenciaWT.java
@@ -8,8 +8,12 @@ public class HerenciaWT {
     public static void main(String[] args) {
         
           Estudiante estudiante1 = new Estudiante();
+          estudiante1.setNombre("Wilmar Tovar");
+          estudiante1.setEdad(16);
+          estudiante1.setCarrera("Ingenieria de Software");
           
-        
+   
+          
     }
     
 }
