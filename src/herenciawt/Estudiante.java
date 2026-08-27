b707a767d1577cff983a6cf15469d60f391bc7a9
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-08-27 14:57:39 -0500

    Creacion de metodos getters y setters para Subclase Estudiante

--- src/herenciawt/Estudiante.java
+++ src/herenciawt/Estudiante.java
@@ -5,5 +5,10 @@ package herenciawt;
 public class Estudiante extends Persona{
     private String carrera="";
     
-    
+    public String getCarrera(){
+        return carrera;
+    }
+    public void setCarrera(String carrera){
+        this.carrera = carrera;
+    }
 }
