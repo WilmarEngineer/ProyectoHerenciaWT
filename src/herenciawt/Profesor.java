efa1a0122b13295f09a5c18ea0d6d3914542bd86
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-08-27 15:02:32 -0500

    Creacion de metodos getters y setters para la Subclase Profesor

--- src/herenciawt/Profesor.java
+++ src/herenciawt/Profesor.java
@@ -3,5 +3,13 @@ package herenciawt;
 
 
 public class Profesor extends Persona {
+   
+    private String asignatura="";
     
+    public String getAsignatura(){
+        return asignatura;
+    }
+   public void setAsignatura(String asignatura){
+       this.asignatura = asignatura;
+   }
 }
