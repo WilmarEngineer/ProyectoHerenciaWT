56e043a3e87fd87b8127b7849357b56149f079ee
Wilmar Tovar <wtovar@uniempresarial.edu.co>
2026-08-27 14:55:09 -0500

    Creacion de atributo privado para Subclase Estudiante

--- src/herenciawt/Estudiante.java
+++ src/herenciawt/Estudiante.java
@@ -1,13 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package herenciawt;
 
-/**
- *
- * @author USER
- */
+
 public class Estudiante extends Persona{
+    private String carrera="";
+    
     
 }
